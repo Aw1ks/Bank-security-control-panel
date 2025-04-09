dc6c8302c2c5ceaf2e5a2201ba20df63167d52f4
Aw1ks <145365323+Aw1ks@users.noreply.github.com>
2025-04-09 13:56:57 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,29 +6,32 @@ from django.shortcuts import render
 from datacenter.common_functions import calculate_visit_duration, is_visit_long
 
 
-def storage_information_view(request):
-    active_visits = Visit.objects.filter(leaved_at=None)
-
+def format_duration(duration):
     number_hours_day = 24
     number_seconds_hour = 3600
     number_seconds_minute = 60
+
+    hours = duration.days * number_hours_day + duration.seconds // number_seconds_hour
+    minutes = (duration.seconds % number_seconds_hour) // number_seconds_minute
+    seconds = duration.seconds % number_seconds_minute
+
+    formatted_duration = f"{hours:02}ч:{minutes:02}м:{seconds:02}с"
+    return formatted_duration
+
+
+def storage_information_view(request):
+    active_visits = Visit.objects.filter(leaved_at=None)
 
     non_closed_visits = []
     
     for visit in active_visits:
         duration = calculate_visit_duration(visit)
 
-        hours = duration.days * number_hours_day + duration.seconds // number_seconds_hour
-        minutes = (duration.seconds % number_seconds_hour) // number_seconds_minute
-        seconds = duration.seconds % number_seconds_minute
-
-        formatted_duration= f"{hours:02}ч:{minutes:02}м:{seconds:02}с"
-        
         non_closed_visits.append(
             {
                 'who_entered': visit.passcard.owner_name,
                 'entered_at': visit.entered_at,
-                'duration': formatted_duration,
+                'duration': format_duration(duration),
                 'is_strange': is_visit_long(visit, minutes=60)
             }
         )

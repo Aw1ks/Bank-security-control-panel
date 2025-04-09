df8368c07253cca95eaac2e54e2d73a7fde05b8a
Aw1ks <145365323+Aw1ks@users.noreply.github.com>
2025-04-09 13:52:37 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,20 +1,7 @@
 from functools import total_ordering
 from datacenter.models import Passcard, Visit
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
-
-def calculate_visit_duration(visit):
-    entered_at = timezone.localtime(visit.entered_at)
-    leaved_at = timezone.localtime(visit.leaved_at) if visit.leaved_at else timezone.now()
-    duration = leaved_at - entered_at
-    return duration
-
-
-def is_visit_long(visit, minutes):
-    number_seconds_minute = 60
-
-    duration = calculate_visit_duration(visit)
-    return duration.total_seconds() >= minutes * number_seconds_minute
+from datacenter.common_functions import calculate_visit_duration, is_visit_long
 
 
 def passcard_info_view(request, passcode):
